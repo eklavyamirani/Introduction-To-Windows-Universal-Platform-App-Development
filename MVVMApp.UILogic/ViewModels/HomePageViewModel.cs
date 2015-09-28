ae5d03418e876798a6118f84c03062e633ee37c8
Eklavya Mirani <eklavyamirani@hotmail.com>
2015-09-28 09:26:51 +0530

    Added Prism based UWP App Demo

---
+++ MVVMApp.UILogic/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Mvvm;
+using Windows.UI.Xaml.Navigation;
+
+namespace MVVMApp.UILogic.ViewModels
+{
+    public class HomePageViewModel : ViewModel
+    {
+        private string title;
+
+        public string Title
+        {
+            get { return this.title; }
+            set { this.SetProperty(ref this.title, value); }
+        }
+
+        public HomePageViewModel()
+        {
+            
+        }
+
+        public override void OnNavigatedTo(object navigationParameter, NavigationMode navigationMode, Dictionary<string, object> viewModelState)
+        {
+            base.OnNavigatedTo(navigationParameter, navigationMode, viewModelState);
+            this.Title = "Welcome to MVVM App!";
+        }
+    }
+}
