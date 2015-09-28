f01e27b337338b293147ce07334560670b2e5f21
Eklavya Mirani <eklavyamirani@hotmail.com>
2015-09-28 11:32:23 +0530

    Added unit tests demo

---
+++ MVVMApp.UnitTests/ViewModelFixtures/HomePageViewModelFixture.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using MVVMApp.UILogic.ViewModels;
+
+namespace MVVMApp.UnitTests.ViewModelFixtures
+{
+    [TestClass]
+    public class HomePageViewModelFixture
+    {
+        private HomePageViewModel homePageViewModel;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            homePageViewModel = new HomePageViewModel();
+        }
+
+        [TestMethod]
+        public void OnNavigatedToHomePage_Should_Set_The_Title()
+        {
+            // Pre conditions
+            Assert.IsTrue(string.IsNullOrEmpty(this.homePageViewModel.Title));
+
+            // Action
+            this.homePageViewModel.OnNavigatedTo(null, Windows.UI.Xaml.Navigation.NavigationMode.New, new Dictionary<string, object>());
+
+            // Validate
+            Assert.IsFalse(string.IsNullOrEmpty(this.homePageViewModel.Title));
+        }
+    }
+}
